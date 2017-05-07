bc07e285605539b5d45a9146f0e70b17e318bd13
qichanna <qichanna@126.com>
2017-05-07 23:32:37 +0800

    first
    first

---
+++ app/src/main/java/home/liqi/calendar/NewCalendar.java
@@ -0,0 +1,24 @@
+package home.liqi.calendar;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by liqi on 2017/5/7.
+ */
+
+public class NewCalendar extends LinearLayout{
+    public NewCalendar(Context context) {
+        super(context);
+    }
+
+    public NewCalendar(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public NewCalendar(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
